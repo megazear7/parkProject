9f3abee47f5108786a8f9a6387155892a7cab513
megazear7 <alexlockhart7@gmail.com>
2014-01-24 17:02:14 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParkProject::Application.config.secret_key_base = '371967f7f2470e3d4e2c5d431ea42584f31be37284abe81b238f1199d36900a0cde031b45936205bf43ddef9e02715ac033a70ba47cbb26e7d9026a55fcfc7f2'
